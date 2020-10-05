8529e1bd7777a0363835f7380517ccaa3c3595d2
ikeng <satidasss@gmail.com>
2020-10-05 15:38:22 +0700

    add blog model editted

--- models/blog.js
+++ models/blog.js
@@ -4,39 +4,39 @@ const User = require('./user');
 
 module.exports = (sequelize, DataTypes) => {
 
-  class Blog extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
+    class Blog extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+        // define association here
+        }
+    };
 
-  User.init(
-    {
-      // id: {
-      //   type: DataTypes.INTEGER,
-      //   autoIncrement: true,
-      //   primaryKey: true
-      // },
-      title: DataTypes.STRING(200),
-      created_at : {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-      } 
-    }, 
-    {
-      sequelize,
-      tableName: 'blogs',
-      modelName: 'Blogs',
-      timestamps: false
-    }
-  );
-  
-  return Blog;
+    Blog.init(
+        {
+        // id: {
+        //   type: DataTypes.INTEGER,
+        //   autoIncrement: true,
+        //   primaryKey: true
+        // },
+        title: DataTypes.STRING(200),
+        created_at : {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        } 
+        }, 
+        {
+        sequelize,
+        tableName: 'blogs',
+        modelName: 'Blogs',
+        timestamps: false
+        }
+    );
+    
+    return Blog;
 };
 
 Blog.belongsTo(User); //Will add userId to Blog model
