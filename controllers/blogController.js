33d973ad415b4818230cc6d0cdadcf439ec26ee3
ikeng <satidasss@gmail.com>
2020-10-06 12:59:54 +0700

    assosiation complete request to run on Windows Dev environment add try

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,21 +2,26 @@ const models = require('../models/index');
 
 exports.index = async (req, res, next) => {
 
-    24.05
-    const blogs = await models.User.findAll({
-        // JOIN
-        include: [{
-            model: models.User,
-            as: 'user',
-            //attributes: ['id', 'title']
-        }],
-        order: [['id', 'desc']]
-    });
+    try{
+        24.05
+        const blogs = await models.User.findAll({
+            // JOIN
+            include: [{
+                model: models.User,
+                as: 'user',
+                //attributes: ['id', 'title']
+            }],
+            order: [['id', 'desc']]
+        });
 
-    res.status(200).json({
-        message: blogs
-    })
-
+        res.status(200).json({
+            message: blogs
+        })
+    }catch(error){
+        res.status(400).json({
+            errorMessage: error
+        })
+    }
 }
 
 
